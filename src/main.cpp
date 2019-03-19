951c875b93e8f54a79482903915b449c847a66d2
Joshua Blustein <blusteinjoshua@gmail.com>
2019-03-19 09:52:47 -0500

    give everything sensible names and use switch statement

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,101 @@
 #include <Arduino.h>
 
 //defines the pin numbers of the speed pin for each motor. Write to these with PWM.
-#define enA1 2
-#define enA2 3
-#define enA3 4
-#define enA4 5
-#define enA5 6
+#define motor1Speed 2
+#define motor2Speed 3
+#define motor3Speed 4
+#define motor4Speed 5
+#define motor5Speed 6
 
 //defines the pin numbers for the direcional pins for each motor.
-#define inA1 23
-#define inB1 25
+#define motor1A 23
+#define motor1B 25
 
-#define inA2 27
-#define inB2 29
+#define motor2A 27
+#define motor2B 29
 
-#define inA3 31
-#define inB3 33
+#define motor3A 31
+#define motor3B 33
 
-#define inA4 35
-#define inB4 37
+#define motor4A 35
+#define motor4B 37
 
-#define inA5 39
-#define inB5 41
+#define motor5A 39
+#define motor5B 41
 
 //declares a variable for motor speed. This can be changed later based on controller inputs.
 int speed = 255;
 
-int r = 1;
+int controllerInput;
 
 void setup() {
   //sets all of the needed pins as outputs
-  pinMode(enA1,OUTPUT);
-  pinMode(enA2,OUTPUT);
-  pinMode(enA3,OUTPUT);
-  pinMode(enA4,OUTPUT);
-  pinMode(enA5,OUTPUT);
+  pinMode(motor1Speed,OUTPUT);
+  pinMode(motor2Speed,OUTPUT);
+  pinMode(motor3Speed,OUTPUT);
+  pinMode(motor4Speed,OUTPUT);
+  pinMode(motor5Speed,OUTPUT);
 
-  pinMode(inA1,OUTPUT);
-  pinMode(inB1,OUTPUT);
+  pinMode(motor1A,OUTPUT);
+  pinMode(motor1B,OUTPUT);
   
-  pinMode(inA2,OUTPUT);
-  pinMode(inB2,OUTPUT);
+  pinMode(motor2A,OUTPUT);
+  pinMode(motor2B,OUTPUT);
 
-  pinMode(inA3,OUTPUT);
-  pinMode(inB3,OUTPUT);
+  pinMode(motor3A,OUTPUT);
+  pinMode(motor3B,OUTPUT);
 
-  pinMode(inA4,OUTPUT);
-  pinMode(inB4,OUTPUT);
+  pinMode(motor4A,OUTPUT);
+  pinMode(motor4B,OUTPUT);
 
-  pinMode(inA5,OUTPUT);
-  pinMode(inB5,OUTPUT);
+  pinMode(motor5A,OUTPUT);
+  pinMode(motor5B,OUTPUT);
   
   Serial.begin(9600);
 }
 
 void loop() {
-  if(Serial.available()) {
-    if(Serial.read() == 0){
+  controllerInput = Serial.read();
+
+  switch (controllerInput)
+  {
+    case 0:
       //forward
-    }
-    if(Serial.read() == 1){
-      //backward
-    }
-    if(Serial.read() == 2){
-      //translate left
-    }
-    if(Serial.read() == 3){
-      //translate right
-    }
-    if(Serial.read() == 4){
-      //rotate clockwise
-    }
-    if(Serial.read() == 5){
-      //rotate counterclockwise
-    }
-    if(Serial.read() == 6){
+      break;
+    
+    case 1:
+      //backwards
+      break;
+
+    case 2:
+      //left
+      break;
+
+    case 3:
+      //right
+      break;
+
+    case 4:
+      //clockwise rotation
+      break;
+
+    case 5:
+      //counterclockwise rotation
+      break;
+
+    case 6:
       //up
-    }
-    if(Serial.read() == 7){
+      break;
+
+    case 7:
       //down
-    }
-    if(Serial.read() == 8){
-      //nuetral/motors off
-    }
-  }
-  else{
-    Serial.print("No serial communications detected");
+      break;
+  
+    case 8:
+      //nuetral/no motor movement
+      
+    default:
+      break;
   }
 }
 
